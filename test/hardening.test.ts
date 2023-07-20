498d7ae1bc5e4ab9ae1573eec4497034be7fca12
Takagi Kensuke <janus.wel.3@gmail.com>
2023-07-20 22:39:26 +0900

    :tada: init

---
+++ test/hardening.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Hardening from '../lib/hardening-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hardening-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Hardening.HardeningStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
